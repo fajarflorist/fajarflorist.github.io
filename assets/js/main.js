8bd94b4b1125bdaabfd5b1c5083b0c410dde335f
degiam <degiam@ymail.com>
2023-05-13 00:44:48 +0700

    Add sticky menu & back to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,41 @@
 /*!
- * Orderio
- * Designed by Degiam [https://degiam.github.io]
+ * Developed by Degiam
+ * https://degiam.github.io
  * Copyright (c) 2023
  */
 
 // Navbar
-	function navbarToggleMenu() {
-		document.getElementById('viewport').classList.toggle('!overflow-hidden')
-		document.querySelector('.navbar').classList.toggle('hidden')
-		let button = document.querySelector('.navbar-toggle')
-		if ( button.getAttribute('aria-expanded') == 'false' ) {
-			button.setAttribute('aria-expanded','true')
-		} else {
-			button.setAttribute('aria-expanded','false')
-		}
+	if ( window.location.href.indexOf('#') > -1 ) {
+		window.location.replace(window.location.href.split('#')[0])
+	}
+
+	function navbarOpenMenu() {
+		document.getElementById('viewport').classList.add('!overflow-hidden')
+		document.querySelector('.navbar').classList.remove('hidden')
+		document.querySelector('.navbar-toggle').setAttribute('aria-expanded','true')
+	}
+	function navbarCloseMenu() {
+		document.getElementById('viewport').classList.remove('!overflow-hidden')
+		document.querySelector('.navbar').classList.add('hidden')
+		document.querySelector('.navbar-toggle').setAttribute('aria-expanded','false')
 	}
 	document.querySelector('.navbar-toggle').onclick = () => {
-		navbarToggleMenu()
+		if ( window.location.href.indexOf('#') > -1 ) {
+			history.back()
+			navbarCloseMenu()
+		} else {
+			window.location.href = '#'
+			navbarOpenMenu()
+		}
 	}
+
+	window.addEventListener('popstate', () => {
+		if ( window.location.href.indexOf('#') > -1 ) {
+			navbarOpenMenu()
+		} else {
+			navbarCloseMenu()
+		}
+	})
 
 	function navbarAutoHeight(selector) {
 		if ( document.body.clientWidth < 768 ) {
@@ -56,10 +74,61 @@
 	}
 	dropdown('.navbar-dropdown','ul')
 
+// Scroll
+	function onScroll(selector,classname,offset) {
+		document.addEventListener('scroll', () => {
+			let element = document.querySelector(selector)
+				scrollTop = document.scrollingElement.scrollTop
+				offsetY = document.body.offsetTop + offset
+			if ( !document.querySelector(selector + '.' + classname) ) {
+				if ( scrollTop >= offsetY ) {
+					element.classList.add(classname)
+				}
+			} else {
+				if ( scrollTop < offsetY ) {
+					element.classList.remove(classname)
+				}
+			}
+		})
+
+		let body = document.getElementById('viewport')
+		body.addEventListener('scroll', () => {
+			let element = document.querySelector(selector)
+				scrollTop = body.scrollTop
+				offsetY = body.offsetTop + offset
+			if ( !document.querySelector(selector + '.' + classname) ) {
+				if ( scrollTop >= offsetY ) {
+					element.classList.add(classname)
+				}
+			} else {
+				if ( scrollTop < offsetY ) {
+					element.classList.remove(classname)
+				}
+			}
+		})
+	}
+	onScroll('html','scroll',100)
+
+// Back to Top
+	function backTop(selector,target) {
+		document.querySelectorAll(selector).forEach(item => {
+			item.addEventListener('click', () => {
+				item.classList.add('active')
+				setTimeout(() => item.classList.remove('active'),500)
+				if ( document.body.clientWidth < 992 ) {
+					document.querySelector(target).scrollTo({behavior:'smooth',top:0})
+				} else {
+					window.scrollTo({behavior:'smooth',top:0})
+				}
+			})
+		})
+	}
+	backTop('.backtop button','#viewport')
+
 // Categories & Tags
 	function filterProduct(path,attr) {
 		let url = window.location.href
-		if ( window.location.href.indexOf(path) > -1 ) {
+		if ( url.indexOf(path) > -1 ) {
 			let query = url.split('?q=')
 			document.querySelectorAll('[' + attr + '="' + query[1].replace(/[+]/g,' ') + '"]').forEach(item => {
 				item.classList.remove('hidden')
